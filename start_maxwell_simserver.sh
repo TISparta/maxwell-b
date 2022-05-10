7e2d30d87c0a76e1747495b3cd17fbe50b127eb5
TISparta <jose.garcia@utec.edu.pe>
2022-05-10 10:23:27 -0500

    (docs) script for running the program

--- start_maxwell_simserver.sh
+++ start_maxwell_simserver.sh
@@ -11,13 +11,9 @@ NGPUS=1
 
 # Main directory for Maxwell source code.
 BASEDIR=.
-# Location of Python virtualenv containing Maxwell dependencies.
-PYENV=venv
 SERVER_DIR=maxwell-server
 PYTHON=python3
 
 cd $BASEDIR
 
-source $PYENV/bin/activate
-$PYTHON $SERVER_DIR/webserver.py $PORT  &> $BASEDIR/webserver.log &
 $PYTHON $SERVER_DIR/simserver.py $NGPUS &> $BASEDIR/simserver.log
---
+++ start_maxwell_webserver.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+# Simple script for starting up maxwell
+
+# Location to store temporary files.
+export MAXWELL_SERVER_FILES=/tmp/maxwell-server-files
+# Port to use for webserver.
+PORT=9041
+# Number of GPUS per solve.
+NGPUS=1
+
+# Main directory for Maxwell source code.
+BASEDIR=.
+SERVER_DIR=maxwell-server
+PYTHON=python3
+
+cd $BASEDIR
+
+$PYTHON $SERVER_DIR/webserver.py $PORT  &> $BASEDIR/webserver.log
